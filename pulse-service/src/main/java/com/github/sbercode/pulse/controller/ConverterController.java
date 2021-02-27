baae1f55f6549962f627401e9bbaca3faea13ca1
Sherzod Mamadaliev <egnaf@yahoo.com>
2021-02-27 18:56:49 +0300

    Fix audio converter response

--- pulse-service/src/main/java/com/github/sbercode/pulse/controller/ConverterController.java
+++ pulse-service/src/main/java/com/github/sbercode/pulse/controller/ConverterController.java
@@ -3,14 +3,19 @@ package com.github.sbercode.pulse.controller;
 import com.github.sbercode.pulse.model.NodeType;
 import com.github.sbercode.pulse.model.Post;
 import com.github.sbercode.pulse.model.node.TextNode;
+import com.github.sbercode.pulse.model.storage.Audio;
+import com.github.sbercode.pulse.repository.StorageRepository;
 import com.github.sbercode.pulse.service.PostService;
 import com.github.sbercode.pulse.util.ClassConverter;
-import lombok.AllArgsConstructor;
-import org.dvp.yask.speechkit.ByteArrayToFile;
+import org.bson.types.Binary;
 import org.dvp.yask.speechkit.ContentConverter;
 import org.dvp.yask.speechkit.speechkit.Voice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
@@ -18,15 +23,26 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 
 @RestController
-@AllArgsConstructor
 public class ConverterController {
 
     private final PostService postService;
+    private final StorageRepository storageRepository;
+
+    @Value("${storagePath}")
+    private String storagePath;
+
+    @Autowired
+    public ConverterController(PostService postService, StorageRepository storageRepository) {
+        this.postService = postService;
+        this.storageRepository = storageRepository;
+    }
 
     //TODO: функция конвертации из короткого текста в аудио. На вход принимает ссылку на статью и имя голоса
-    @GetMapping("/converter/voice/{id}/{nameVoice}")
-    public String shortTextToVoice(@PathVariable String id, @PathVariable String nameVoice) {
+    @GetMapping(value = "/converter/voice/{nameVoice}/{id}.ogg",
+            produces = "audio/mpeg")
+    public @ResponseBody byte[] shortTextToVoice(@PathVariable String id, @PathVariable String nameVoice) {
         String fileName = "undefiled";
+        byte[] savedFile;
         ContentConverter contentConverter = new ContentConverter();
 
         byte[] result;
@@ -42,17 +58,20 @@ public class ConverterController {
                 .map(TextNode::getValue)
                 .collect(Collectors.joining());
 
-        result = contentConverter.shortTextToVoice(text, Voice.FILIPP);
+        result = contentConverter.shortTextToVoice(text, nameVoice);
 
         try {
             //TODO: тут создаем файл. Имя неважно. предлагаю по id статьи
-            fileName = "/files/audio_" + UUID.randomUUID().toString() + ".ogg";
-            ByteArrayToFile.save(fileName, result);
+            fileName = storagePath + "audio_" + UUID.randomUUID().toString() + ".ogg";
+            //ByteArrayToFile.save(fileName, result);
+
+            Audio savedAudio = storageRepository.save(Audio.builder().file(new Binary(result)).build());
+            if (savedAudio.getFile() == null) throw new RuntimeException("Audio was not saved");
+            savedFile = savedAudio.getFile().getData();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        //TODO: вернуть ссылку на файл
-        return fileName;
+        return savedFile;
     }
 
     //TODO: функция конвертации аудио в текст. На вход принимает ссылку на статью
